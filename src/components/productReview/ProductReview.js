54629ee28c03e233c140edfe6d21707c7deaca18
Tazwar <tazwar9t6@gmail.com>
2020-08-31 09:39:15 +0600

    done adding review

--- src/components/productReview/ProductReview.js
+++ src/components/productReview/ProductReview.js
@@ -1,13 +1,21 @@
 import React from "react";
 
 const ProductReview = (props) => {
-  console.log(props.product);
-  console.log(props.product.name);
+  // console.log(props.product);
+  // console.log(props.product.name);
   let { name, quantity } = props.product;
+  let reviewStyle = {
+    margin: "10px",
+    paddingLeft: "150px",
+    borderBottom: "1px solid lightgray",
+    paddingBottom: "10px",
+  };
   return (
-    <div>
-      <h2>{name}</h2>
-      <h1>Quantity: {quantity}</h1>
+    <div style={reviewStyle}>
+      <h2 className="product-name">{name}</h2>
+      <p>Quantity: {quantity}</p>
+      <br />
+      <button className="buy-button">Remove</button>
     </div>
   );
 };
