f7b2c166492fad3804bc2832e5e7002156348450
Tazwar <tazwar9t6@gmail.com>
2020-08-31 16:45:58 +0600

    ummm done

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -3,18 +3,32 @@ import { useState, useEffect } from "react";
 import {
   getDatabaseCart,
   removeFromDatabaseCart,
+  processOrder,
 } from "../../utilities/databaseManager";
 import fakeData from "../../fakeData";
 import ProductReview from "../productReview/ProductReview";
 import Cart from "../Cart/Cart";
+import thanks from "../../images/thanks.gif";
 
 const Review = () => {
   let [cart, setCart] = useState([]);
+  let [orderPlaced, setOrderPlaced] = useState(false);
+  let placeOrder = () => {
+    setOrderPlaced(true);
+    setCart([]);
+    processOrder();
+  };
+
   let removeButton = (productKey) => {
     let newCart = cart.filter((pd) => pd.key !== productKey);
     setCart(newCart);
     removeFromDatabaseCart(productKey);
   };
+  let thanksGIF;
+  if (orderPlaced) {
+    thanksGIF = <img src={thanks} alt="" />;
+  }
+
   useEffect(() => {
     let savedCart = getDatabaseCart();
     let productKeys = Object.keys(savedCart);
@@ -35,9 +49,14 @@ const Review = () => {
             removeButton={removeButton}
           ></ProductReview>
         ))}
+        {thanksGIF}
       </div>
       <div className="cart-container">
-        <Cart cart={cart}></Cart>
+        <Cart cart={cart}>
+          <button onClick={placeOrder} className="buy-button">
+            Place Order
+          </button>
+        </Cart>
       </div>
     </div>
   );
